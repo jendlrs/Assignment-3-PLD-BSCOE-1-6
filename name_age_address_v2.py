b15b687c6a6530a35c46adbf384f49a5ebaab582
jendlrs <jensennics.dlrs@gmail.com>
2021-10-30 20:02:52 +0800

    draft

---
+++ name_age_address_v2.py
@@ -0,0 +1,9 @@
+def getName():
+    nameA=input ("\nWhat is your name?\n>")
+    return nameA
+def getAge():
+    ageA= input ("\nHow old are you?\n>")
+    return ageA
+
+name= getName()
+age=getAge()
