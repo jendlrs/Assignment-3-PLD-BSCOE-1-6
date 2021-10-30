4e5762185bbf611323aba51f423ee506fb3b7dc1
jendlrs <jensennics.dlrs@gmail.com>
2021-10-31 00:06:22 +0800

    added some def

--- name_age_address_v2.py
+++ name_age_address_v2.py
@@ -1,9 +1,20 @@
 def getName():
-    nameA=input ("\nWhat is your name?\n>")
+    nameA =input ("\nWhat is your name?\n>")
     return nameA
+
 def getAge():
-    ageA= input ("\nHow old are you?\n>")
+    ageA=input ("\nHow old are you?\n>")
     return ageA
+
+def getAddress():
+    addressA=input ("\nWhere do you live?\n>")
+    return addressA
+
+def display (nameA, ageA, addressA):
+    print (f"Hi, my name is {nameA}. I am {ageA} years old and I live in {addressA}.")
 
 name= getName()
 age=getAge()
+address=getAddress()
+
+display(name, age, address)
