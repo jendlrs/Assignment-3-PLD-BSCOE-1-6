70ba45169ee92c8f906fade137856de9e8455b42
jendlrs <jensennics.dlrs@gmail.com>
2021-10-31 12:33:37 +0800

    2 functions run

---
+++ quantity_max_apple_v2.py
@@ -0,0 +1,9 @@
+def getMoney():
+    moneyA= float(input("\n Enter the amount of money you have: \n> "))
+    return moneyA
+def getCostForApple ():
+    cost_per_appleA= float(input("\n Price per apple: \n> "))
+    return cost_per_appleA
+
+money=getMoney()
+cost_per_apple= getCostForApple ()
