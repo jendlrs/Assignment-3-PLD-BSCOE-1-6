b0c4aca2f9cb70ce5e6494af86a1a30168fafdf2
jendlrs <jensennics.dlrs@gmail.com>
2021-10-31 13:12:50 +0800

    update

--- quantity_max_apple_v2.py
+++ quantity_max_apple_v2.py
@@ -1,8 +1,8 @@
 def getMoney():
-    moneyA= float(input("\n Enter the amount of money you have: \n> "))
+    moneyA= float(input("\nEnter the amount of money you have: \n> "))
     return moneyA
 def getCostForApple ():
-    cost_per_appleA= float(input("\n Price per apple: \n> "))
+    cost_per_appleA= float(input("\nPrice per apple: \n> "))
     return cost_per_appleA
 def computeForMaxQuantity ():
     max_quantityA= int(money/cost_per_apple)
@@ -10,8 +10,11 @@ def computeForMaxQuantity ():
 def computeForChange():
     changeA= money % cost_per_apple
     return changeA
+def display(max_quantityA, changeA):
+    print(f"\nYou can buy {max_quantityA} apples and your change is {changeA:.2f} pesos. \n")
 
 money=getMoney()
 cost_per_apple= getCostForApple ()
 max_quantity= computeForMaxQuantity ()
 change= computeForChange ()
+display(max_quantity,change)
